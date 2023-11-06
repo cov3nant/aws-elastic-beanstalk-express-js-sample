4b7151f3315459fa8812bca9b1a4b924e647c02f
cov3nant <godswayagetor@gmail.com>
2023-11-06 10:07:39 +0000

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('this is stressful'));
+app.get('/', (req, res) => res.send('Well, I did it!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
